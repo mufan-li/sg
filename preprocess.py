91b7374548677d4749ae8244c5a5763323133ed5
Mufan (Bill) Li <mufan.li@outlook.com>
2015-09-17 17:21:11 -0400

    preprocess data

--- preprocess.py
+++ preprocess.py
@@ -13,8 +13,10 @@ print 'number of students: ' + str(n_students)
 print 'number of courses: ' + str(n_courses)
 print 'number of grades: ' + str(n_grades) + '\n'
 
+#################################
 # aggregated data by student
 #  - count the number of courses taken per student
+
 groupById = sgdata[['ID','GRADE']].groupby('ID')
 aggById = groupById.count().add_suffix('_count').reset_index()
 
@@ -23,18 +25,51 @@ aggByIdCount = groupByIdCount.count().add_suffix('_count').reset_index()
 
 aggByIdCount['c_pct'] = np.cumsum(aggByIdCount['ID_count']) / n_students
 
-print aggByIdCount
+# print aggByIdCount
 
+#################################
 # aggregate data by course
 #  - count the number of students enrolled per course
+
 groupByCourse = sgdata[['ID','COURSE']].groupby('COURSE')
 aggByCourse = groupByCourse.count().add_suffix('_count').reset_index()
 
 groupByCourseCount = aggByCourse.groupby('ID_count')
-aggByCourseCount = groupByCourseCount.count().add_suffix('_count')
-aggByCourseCount = aggByCourseCount.reset_index()
+aggByCourseCount = groupByCourseCount.count().add_suffix('_count').\
+					reset_index()
 
 aggByCourseCount['c_pct'] = np.cumsum(aggByCourseCount['COURSE_count'])\
 								/ n_courses
 
-print aggByCourseCount
+# print aggByCourseCount
+
+#################################
+# take only the data interested #
+#################################
+
+idFilter = aggById[aggById['GRADE_count']>=10]['ID'].unique()
+courseFilter = aggByCourse[aggByCourse['ID_count']>=20]['COURSE'].\
+				unique()
+
+sgdata2 = sgdata[ ( sgdata['ID'].isin(idFilter) ) & \
+		( sgdata['COURSE'].isin(courseFilter) ) ]
+
+# size of data
+n_students2 = len(sgdata2['ID'].unique())
+n_courses2 = len(sgdata2['COURSE'].unique())
+n_grades2 = len(sgdata2)
+
+print 'after filter: '
+print 'number of students: ' + str(n_students2) + ', ' + \
+		str(n_students2*1.0 / n_students)
+print 'number of courses: ' + str(n_courses2) + ', ' + \
+		str(n_courses2*1.0 / n_courses)
+print 'number of grades: ' + str(n_grades2) + ', ' + \
+		str(n_grades2*1.0 / n_grades) + '\n'
+
+
+
+
+
+
+
